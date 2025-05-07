6519282b0b429253e990ef7526dcca867d7a0e3a
phuy1125 <phuonghuy11252004@gmail.com>
2025-05-07 23:46:18 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['scontent.fsgn2-3.fna.fbcdn.net'], // Add the domain to the list
+    domains: ['scontent.fsgn2-3.fna.fbcdn.net'], // Thêm domain vào danh sách
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Tắt kiểm tra ESLint trong quá trình build
   },
 };
 
